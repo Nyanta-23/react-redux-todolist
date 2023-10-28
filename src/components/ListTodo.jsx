2e81d1efa186ac81f22df7df3fa1fc61d30bfcb9
Nyanta-23 <muhamadilhan02404@gmail.com>
2023-10-28 15:56:09 +0700

    resolving style on list todo components

--- src/components/ListTodo.jsx
+++ src/components/ListTodo.jsx
@@ -1,20 +1,23 @@
-import React from 'react';2
+import React from "react";
+2;
 
 function ListTodo() {
   return (
-    <section className='flex justify-center'>
-      <ul className='flex flex-col mt-5 gap-3'>
-        <li>
-          <div className='flex border w-fit font-semibold text-base rounded-sm'>
-            <input type="checkbox" className='' />
-            <h5>Buy Milk</h5>
-            <button>Edit</button>
-            <button>Delete</button>
+    <section className="flex justify-center">
+      <ul className="flex flex-col mt-5 gap-3">
+        <li className="flex border font-semibold text-base rounded-sm align-middle px-2 py-2 justify-between gap-20 border-gray-300">
+          <div className="flex gap-3">
+            <input type="checkbox" className="w-5 cursor-pointer border-none" />
+            <h5 className="text-lg text-left">Buy Milk</h5>
+          </div>
+          <div className="flex gap-2">
+            <button>✏️</button>
+            <button>🗑️</button>
           </div>
         </li>
       </ul>
     </section>
-  )
+  );
 }
 
 export default ListTodo;

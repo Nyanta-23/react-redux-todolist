280fb18c8a5f3ae70476123cc45197494da9c0ec
Nyanta-23 <muhamadilhan02404@gmail.com>
2023-10-28 15:58:14 +0700

    resolving margin on navigtaion component

--- src/components/NavigationTodo.jsx
+++ src/components/NavigationTodo.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function NavigationTodo() {
   return (
-    <section className="">
+    <section className="mt-5">
       <ul className="flex justify-center gap-3 text-sm">
         <li>
           <button className="bg-violet-600 text-gray-50 px-5 py-2 rounded-full">All</button>
